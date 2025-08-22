acab32d9818dceea69cf469ad3a88c73191b46c8
Vitalii Levda <vitalii.levda@tul.cz>
2025-08-22 14:16:12 +0300

    first try: added checks for directions

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -1,7 +1,27 @@
 package core.basesyntax;
 
 public class RobotRoute {
+
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+
+        if (robot.getX() != toX) {
+            turnRobotToDirection(robot, robot.getX() < toX ? Direction.RIGHT : Direction.LEFT);
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+        }
+
+        if (robot.getY() != toY) {
+            turnRobotToDirection(robot, robot.getY() < toY ? Direction.UP : Direction.DOWN);
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+        }
+    }
+
+    private void turnRobotToDirection(Robot robot, Direction targetDirection) {
+        while (robot.getDirection() != targetDirection) {
+            robot.turnRight();
+        }
     }
 }
